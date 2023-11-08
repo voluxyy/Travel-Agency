5862c9e5a091021ffd45f34cfc7a16f8896b0782
voluxyy <lasserre.anael2@gmail.com>
2023-11-08 18:06:43 +0100

    ADD New component to display destination

---
+++ front-travel-agency/src/app/destination/destination.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-destination',
+  templateUrl: './destination.component.html',
+  styleUrls: ['./destination.component.sass']
+})
+export class DestinationComponent {
+
+}
