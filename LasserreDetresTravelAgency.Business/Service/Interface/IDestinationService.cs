89ea3692248e15419a75e2df097170ada820140d
voluxyy <lasserre.anael2@gmail.com>
2023-11-21 15:47:33 +0100

    ADD Comments for new method

--- LasserreDetresTravelAgency.Business/Service/Interface/IDestinationService.cs
+++ LasserreDetresTravelAgency.Business/Service/Interface/IDestinationService.cs
@@ -45,6 +45,14 @@ namespace LasserreDetresTravelAgency.Business.Service
         /// <returns>Returns a list of visited destination data.</returns>
         List<DestinationDto> GetAllVisited();
 
+        /// <summary>
+        /// Retrieves a list of destination data transfer objects (DTOs) based on a user's ID and a specified category.
+        /// </summary>
+        /// <param name="userId">The ID of the user for whom destinations are being retrieved.</param>
+        /// <param name="CategoryId">The ID of the category to filter destinations.</param>
+        /// <returns>
+        /// Returns a list of destination DTOs representing destinations that match the specified category and have been visited by the user.
+        /// </returns>
         List<DestinationDto> GetAllDestinationByUserAndCategory(int userId, int CategoryId);
     }
 }
